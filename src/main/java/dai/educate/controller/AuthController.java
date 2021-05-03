65c00ee8741bfd022284615e1d711c415954d744
Your Name <a92915@alnos.uminho.pt>
2021-05-03 11:40:56 +0100

    Login

---
+++ src/main/java/dai/educate/controller/AuthController.java
@@ -0,0 +1,45 @@
+package dai.educate.controller;
+/*
+import daibackend.demo.model.custom.LoginRequest;
+import daibackend.demo.payload.response.ApiResponse;
+import daibackend.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
+                                              HttpServletResponse response) {
+        return authService.authenticateUser(loginRequest, response);
+    }
+
+
+    @GetMapping("/logout")
+    public ResponseEntity<ApiResponse> logoutUser(HttpServletRequest request, HttpServletResponse response) {
+        return authService.logoutUser(request, response);
+    }
+
+
+
+}
+*/

dec00fd6627d02d481d41d17a48630cb22c24abe
Your Name <a92915@alnos.uminho.pt>
2021-05-09 17:39:25 +0100

    report

--- src/main/java/dai/educate/repository/ChildRepository.java
+++ src/main/java/dai/educate/repository/ChildRepository.java
@@ -3,11 +3,9 @@ package dai.educate.repository;
 import dai.educate.model.Child;
 import dai.educate.model.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
 
 @Repository
